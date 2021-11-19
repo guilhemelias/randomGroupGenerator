b9a5f6294a23a29c6b5e3c1deb5c02eac53d194b
guilhemelias <56593283+guilhemelias@users.noreply.github.com>
2021-11-19 06:58:14 +0100

    [DEV] beginning of randomize part

--- randomGroupProject/controllers/ControleurClass.php
+++ randomGroupProject/controllers/ControleurClass.php
@@ -6,6 +6,12 @@
 
 class ControleurClass {
 
+   private  $O_class;
+
+   public function __construct(){
+      $this->O_class= new ClassStudent();
+   }
+
    public function defautAction()
    {
    
@@ -21,10 +27,10 @@ class ControleurClass {
             if($extension == 'csv')
             {
                $file = $_FILES['file']['tmp_name'];
-               $O_class=new ClassStudent();
-               $O_class->getStudent($file);
+               
+               $this->O_class->getStudent($file);
                $error='';
-               Vue::montrer('helloworld/class', array('class' =>  $O_class->displayClass($O_class->getListStudent()),'error'=>$error));            
+              // Vue::montrer('helloworld/class', array('class' =>  $this->O_class->displayClass($this->O_class->getListStudent()),'error'=>$error));            
             }
             else
             {
@@ -38,6 +44,15 @@ class ControleurClass {
     //  $O_class->getStudent($file_data);
     //  Vue::montrer('helloworld/class', array('class' =>  $O_class->displayClass($O_class->getListStudent()),'error'=>$error));
 
+   }
+
+
+
+   public function randomizeAction(){
+      
+      echo $this->O_class;
+      
+      Vue::montrer('helloworld/class', array('class' =>  $this->O_class->displayClass($this->O_class->getListStudent()),'error'=>$this->error));
    }
 
    
